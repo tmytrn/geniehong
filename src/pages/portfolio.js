7a197f76890e25dffeef9d0d36c8573db9c1b095
Tommy Tran <tommy@tmytrn.com>
2023-04-04 23:52:26 -0500

    fix portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { getPortfolioData, getPortfolioPass } from "../../data";
 import { useState, useRef } from "react";
 
-export default function Portfolio({ data, pwd }) {
+export default function Portfolio({ data, pwd, metaData }) {
   const [isUnlocked, setIsUnlocked] = useState(false);
   const [errorText, setErrorText] = useState("");
   const pwdRef = useRef(null);
@@ -28,24 +28,79 @@ export default function Portfolio({ data, pwd }) {
         <meta property="og:title" content={metaData?.title} key="title" />
         <meta property="og:description" content={metaData?.description} />
         <meta property="og:image" content={metaData?.previewImage.asset.url} />
-        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
-<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
-<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
-<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
-<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
-<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
-<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
-<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
-<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-<link rel="manifest" href="/manifest.json"/>
-<meta name="msapplication-TileColor" content="#ffffff"/>
-<meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
-<meta name="theme-color" content="#ffffff"/>
-
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <main className="w-full h-full bg-gray-200">
         {isUnlocked ? (
@@ -105,11 +160,13 @@ export default function Portfolio({ data, pwd }) {
 export async function getStaticProps({ params, preview = false }) {
   const result = await getPortfolioData();
   const pwd = await getPortfolioPass();
+  const metaData = await getMetaData();
 
   return {
     props: {
       data: result[0],
       pwd: pwd[0],
+      metaData: metaData[0],
       page: "portfolio",
       preview,
     },
